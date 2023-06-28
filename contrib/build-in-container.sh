f6eac92a33cc661519b49ed87288c2ffadecd21c
Daniel Lublin <daniel@lublin.se>
2023-06-28 08:08:01 +0200

    Copy app.bin from container build to host

--- contrib/build-in-container.sh
+++ contrib/build-in-container.sh
@@ -7,32 +7,32 @@ scriptf="$(mktemp)"
 
 cat >"$scriptf" <<EOF
 #!/bin/bash
-mkdir -p /src
-cd /src
-cp -af /age-plugin-tkey .
-git clone --depth=1 --branch=v0.0.1 https://github.com/tillitis/tkey-libs
+mkdir /src
+cp -af /age-plugin-tkey /src/
+git -C /src clone --depth=1 --branch=v0.0.1 https://github.com/tillitis/tkey-libs
 # TODO change to main once merged
-git clone --depth=1 --branch=rework https://github.com/quite/tkey-device-x25519
+git -C /src clone --depth=1 --branch=rework https://github.com/quite/tkey-device-x25519
 make -C /src/tkey-libs -j
 make -C /src/tkey-device-x25519 -j
 make -C /src/age-plugin-tkey clean
 make -C /src/age-plugin-tkey -j age-plugin-tkey
 cp -af /src/age-plugin-tkey/age-plugin-tkey /age-plugin-tkey/
+cp -af /src/tkey-device-x25519/x25519/app.bin /age-plugin-tkey/internal/tkey/
 EOF
 
 chmod +x "$scriptf"
 
-cached=/tmp/age-plugin-tkey-build-cache
-mkdir -p $cached/{go,dotcache}
+cached=/tmp/age-plugin-tkey-build-cache-$UID
+mkdir -p $cached/{go,.cache}
 
-podman run --rm -it \
-       --mount type=bind,source=$PWD,destination=/age-plugin-tkey \
-       --mount type=bind,source=$scriptf,destination=/inside.sh \
-       --mount type=bind,source=$cached/go,destination=/root/go \
-       --mount type=bind,source=$cached/dotcache,destination=/root/.cache \
-       tkey-apps-builder \
-       /inside.sh
+podman run --rm -i -t \
+ --mount "type=bind,source=$PWD,destination=/age-plugin-tkey" \
+ --mount "type=bind,source=$scriptf,destination=/inside.sh" \
+ --mount "type=bind,source=$cached/go,destination=/root/go" \
+ --mount "type=bind,source=$cached/.cache,destination=/root/.cache" \
+ tkey-apps-builder \
+ /inside.sh
 
 rm -f "$scriptf"
 
-ls -l "$(realpath ./age-plugin-tkey)"
+ls -l "$(realpath ./age-plugin-tkey)" "$(realpath ./internal/tkey/app.bin)"
